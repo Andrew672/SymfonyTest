bfff4a246a604318f24c35c5aa10e5eca46dba5a
Andrew Marbach <andrew.marbach1@etu.univ-lorraine.fr>
2022-11-24 03:48:34 +0100

    Ajout de la traduction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(TranslatorInterface $translator): Response
     {
+
+        $message = $translator->trans("Hello World");
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'message' => $message
         ]);
+    }
+
+    /**
+     * @Route("/change-locale/{locale}", name="change_locale")
+     */
+
+    public function changeLocale($locale, Request $request)
+    {
+        $request->getSession()->set('_locale', $locale);
+
+        return $this->redirect($request->headers->get('referer'));
+
+        
     }
 }
